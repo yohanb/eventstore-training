0a5292b4d7dbc94aafc66cc9a847e7738198d17b
chris.condron <chris@condron.net>
2019-08-21 16:11:34 -0700

    add bootstrap

--- src/web/web/Startup.cs
+++ src/web/web/Startup.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json;
+using Registration.Blueprint.Commands;
+using HotelDomain =  Registration.Application.Bootstrap;
 
 namespace web
 {
@@ -13,6 +15,7 @@ namespace web
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            HotelDomain.AsService();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,7 +29,7 @@ namespace web
             app.Run(async (context) =>
             {
                 var page = context.Request.Path.ToUriComponent();
-                if (page.EndsWith(".html")) RouteToPage(page, context);
+                if (page.EndsWith(".html") || page.Equals("/")) RouteToPage(page, context);
                 else RouteToCommandHandler(page, context);
             });
         }
@@ -34,7 +37,7 @@ namespace web
         private static void RouteToCommandHandler(string commandHandler, HttpContext context) {
             switch (commandHandler) {
                 case "book-room":
-                    //.HandleCommand(new BookRoomCommand());
+                    HotelDomain.MainBus.Publish(new BookRoomCmd());
                     break;
                 default:
                     context.Response.SendFileAsync("401.html");
@@ -43,13 +46,14 @@ namespace web
         }
 
         private static void RouteToPage(string page, HttpContext context) {
-            if (context.Request.Path == "/")
+            if (context.Request.Path == "/" || page == "/")
             {
-                context.Response.WriteAsync(File.ReadAllText("pages/index.html"));        
+                context.Response.WriteAsync(File.ReadAllText("pages/index.html"));
+                return;
             }
 
 
-            var pageContents = File.ReadAllText(page);
+            //var pageContents = File.ReadAllText(page);
                 
         }
     }
