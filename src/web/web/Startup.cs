49ef643371786bb652d92b85fcd189a4cb369492
Adam Dymitruk <adam.dymitruk@advancetech.io>
2019-08-21 17:11:13 -0700

    small clean up

--- src/web/web/Startup.cs
+++ src/web/web/Startup.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
-using Newtonsoft.Json;
 
 namespace web
 {
